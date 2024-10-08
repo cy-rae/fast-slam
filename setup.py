2d8dd4be3f2eba9e7e9341a4f20a2eefa005a46b
NDI6FE <cy.rae@yahoo.com>
2024-10-09 01:29:21 +0200

    Adjusted algorithm

---
+++ setup.py
@@ -0,0 +1,14 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name='FastSLAM2',
+    version='0.1',
+    packages=find_packages(),
+    description='This module contains all packages for the FastSLAM 2.0 algorithm.',
+    author='Dario Nieddu',
+    author_email='cas402688@cas.dhbw.de',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
