4056ae29b7dd8b2682440187fd12a879edb170d6
NDI6FE <cy.rae@yahoo.com>
2024-10-10 23:34:48 +0200

    Fixed bug

--- fast_slam_2/config.py
+++ fast_slam_2/config.py
@@ -8,11 +8,11 @@ NUM_PARTICLES = 100
 
 # Translation and rotation noise represent the standard deviation of the translation and rotation.
 # The noise is used to add uncertainty to the movement of the robot and particles.
-TRANSLATION_NOISE = 0.0015
-ROTATION_NOISE = 0.0015
+TRANSLATION_NOISE = 0.002
+ROTATION_NOISE = 0.002
 
 # The measurement noise of the Kalman filter depends on the laser's accuracy
 MEASUREMENT_NOISE = np.array([[0.01, 0.0], [0.0, 0.01]])
 
 # Distance threshold which is used to associate a landmark with an observation
-MAXIMUM_LANDMARK_DISTANCE = 2
+MAXIMUM_LANDMARK_DISTANCE = 2.2
