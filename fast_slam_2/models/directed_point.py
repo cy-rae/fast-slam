629912c41661788f2109579d1da94f0e146c55ec
NDI6FE <cy.rae@yahoo.com>
2024-10-18 01:32:43 +0200

    Adjusted point and directed point

--- fast_slam_2/models/directed_point.py
+++ fast_slam_2/models/directed_point.py
@@ -7,10 +7,20 @@ class DirectedPoint(Point):
     """
 
     def __init__(self, x: float, y: float, yaw: float):
+        """
+        Initialize the directed point with the passed parameters.
+        :param x: The x coordinate of the directed point
+        :param y: The y coordinate of the directed point
+        :param yaw: The angle of the directed point in radians
+        """
         super().__init__(x, y)
         self.yaw = yaw
 
     def to_dict(self):
+        """
+        Convert the directed point to a dictionary.
+        :return: Returns the dictionary representation of the directed point.
+        """
         return {
             'x': self.x,
             'y': self.y,
--- fast_slam_2/models/point.py
+++ fast_slam_2/models/point.py
@@ -7,17 +7,26 @@ class Point:
     """
 
     def __init__(self, x: float, y: float):
+        """
+        Initialize the point with the passed variables.
+        :param x: The x coordinate of the point
+        :param y: The y coordinate of the point
+        """
         self.x = x
         self.y = y
 
     def as_vector(self):
         """
-        Get the pose/mean of the point as a vector [x, y].
-        :return: Returns the position of the point as a numpy array [x, y]
+        Get the point as a vector [x, y].
+        :return: Returns the point as a Nx2 array [x, y]
         """
         return np.array([self.x, self.y])
 
     def to_dict(self):
+        """
+        Convert the point to a dictionary.
+        :return: Returns the dictionary representation of the point.
+        """
         return {
             'x': self.x,
             'y': self.y,
