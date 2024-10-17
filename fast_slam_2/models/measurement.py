ff9015bfd4cf3d699422496515f5e82e5fde5a88
NDI6FE <cy.rae@yahoo.com>
2024-10-18 01:35:17 +0200

    Adjusted measurement.py

--- fast_slam_2/models/measurement.py
+++ fast_slam_2/models/measurement.py
@@ -7,8 +7,17 @@ class Measurement:
     """
 
     def __init__(self, distance: float, yaw: float):
+        """
+        Initialize the measurement with the passed parameters.
+        :param distance: The distance to the landmark
+        :param yaw: The angle to the landmark in radians
+        """
         self.distance = distance
         self.yaw = yaw
 
     def as_vector(self):
+        """
+        Get the measurement as a vector [distance, yaw].
+        :return: Returns the measurement as a Nx2 array [distance, yaw]
+        """
         return np.array([self.distance, self.yaw])
