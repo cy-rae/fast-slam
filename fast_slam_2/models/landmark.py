392651fe2eab904dfcdd66d5abbd27e3fe48f3fd
NDI6FE <cy.rae@yahoo.com>
2024-10-18 01:34:43 +0200

    Adjusted landmark.py

--- fast_slam_2/models/landmark.py
+++ fast_slam_2/models/landmark.py
@@ -11,8 +11,18 @@ class Landmark(Point):
     """
 
     def __init__(self, x: float, y: float, cov: ndarray = np.array([[0.1, 0], [0, 0.1]])):
+        """
+        Initialize the landmark with the passed parameters.
+        :param x: The x coordinate of the landmark
+        :param y: The y coordinate of the landmark
+        :param cov: The covariance matrix of the landmark. Default is a 2x2 matrix with 0.1 on the diagonal.
+        """
         super().__init__(x, y)
         self.cov = cov
 
     def __str__(self):
+        """
+        Get the string representation of the landmark.
+        :return: Returns the string representation of the landmark.
+        """
         return f"Landmark ID: x: {self.x}, y: {self.y}, Covariance: {self.cov}"
