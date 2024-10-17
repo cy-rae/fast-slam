b2c01ea3b22fb3d2652271a6e99126713694f233
NDI6FE <cy.rae@yahoo.com>
2024-10-18 01:29:24 +0200

    Adjusted line_filter.py

--- fast_slam_2/algorithms/line_filter.py
+++ fast_slam_2/algorithms/line_filter.py
@@ -4,13 +4,17 @@ from scipy import ndimage
 
 
 class LineFilter:
+    """
+    The LineFilter class is used to filter points to reduce noise.
+    """
+
     @staticmethod
     def filter(points: ndarray, sigma=0.1):
         """
         Apply a Gaussian filter to the points to reduce noise.
         :param points: The points to filter
-        :param sigma: The standard deviation of the Gaussian filter
-        :return: Returns the filtered points
+        :param sigma: The standard deviation of the Gaussian filter. Default is 0.1.
+        :return: Returns the filtered points.
         """
         x_filtered = ndimage.gaussian_filter1d(points[:, 0], sigma=sigma)
         y_filtered = ndimage.gaussian_filter1d(points[:, 1], sigma=sigma)
